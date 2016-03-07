81b4f7d624f5a3c7671c4abc7f5d8a9d9df6d868
Josh Bowman <15678661+bowmanjosh@users.noreply.github.com>
2016-03-07 15:43:45 -0600

    DetailActivity activity added (skeleton), will be used to show detail of weather data from specific day when user taps on the day in the main ForecastFragment list. Added Toast popup when user taps on day for now, will be expanded to launch the DetailActivity.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -92,6 +94,13 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(), mForecastAdapter.getItem(i), Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
 
         return rootView;
     }
@@ -114,8 +123,7 @@ public class ForecastFragment extends Fragment {
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
 
-            String highLowStr = roundedHigh + " / " + roundedLow;
-            return highLowStr;
+            return roundedHigh + " / " + roundedLow;
         }
 
         /**
