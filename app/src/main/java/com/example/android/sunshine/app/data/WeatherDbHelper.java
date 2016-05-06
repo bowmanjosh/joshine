35dbadc8974c536b9fffca77a463c7cb6bdd6ce9
Josh Bowman <15678661+bowmanjosh@users.noreply.github.com>
2016-05-06 15:42:41 -0500

    Fix and clean-up AOSP's code for WeatherDbHelper.

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherDbHelper.java
@@ -27,7 +27,7 @@ import com.example.android.sunshine.app.data.WeatherContract.WeatherEntry;
  */
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
-  // If you change the database schema, you must increment the database version.
+  // DATABASE_VERSION must be incremented when the database schema changes.
   private static final int DATABASE_VERSION = 2;
 
   static final String DATABASE_NAME = "weather.db";
@@ -38,48 +38,48 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
   @Override
   public void onCreate(SQLiteDatabase sqLiteDatabase) {
-    final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
-        // Why AutoIncrement here, and not above?
-        // Unique keys will be auto-generated in either case.  But for weather
-        // forecasting, it's reasonable to assume the user will want information
-        // for a certain date and all dates *following*, so the forecast data
-        // should be sorted accordingly.
+    final String SQL_CREATE_WEATHER_TABLE =
+        "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
+
+        /* Why AutoIncrement here, and not above? Unique keys will be
+         * auto-generated in either case. But for weather forecasting, it's
+         * reasonable to assume the user will want information for a certain
+         * date and all dates *following* it, so the forecast data should be
+         * sorted accordingly. */
         WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
 
-        // the ID of the location entry associated with this weather data
-        WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
-        WeatherEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
-        WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
-        WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL," +
+            // the ID of the location that's associated with this weather data
+            WeatherEntry.COL_LOC_KEY + " INTEGER NOT NULL, " +
 
-        WeatherEntry.COLUMN_MIN_TEMP + " REAL NOT NULL, " +
-        WeatherEntry.COLUMN_MAX_TEMP + " REAL NOT NULL, " +
-
-        WeatherEntry.COLUMN_HUMIDITY + " REAL NOT NULL, " +
-        WeatherEntry.COLUMN_PRESSURE + " REAL NOT NULL, " +
-        WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, " +
-        WeatherEntry.COLUMN_DEGREES + " REAL NOT NULL, " +
+            WeatherEntry.COL_DATE + " INTEGER NOT NULL, " +
+            WeatherEntry.COL_SHORT_DESC + " TEXT NOT NULL, " +
+            WeatherEntry.COL_WEATHER_ID + " INTEGER NOT NULL," +
+            WeatherEntry.COL_MIN_TEMP + " REAL NOT NULL, " +
+            WeatherEntry.COL_MAX_TEMP + " REAL NOT NULL, " +
+            WeatherEntry.COL_HUMIDITY + " REAL NOT NULL, " +
+            WeatherEntry.COL_PRESSURE + " REAL NOT NULL, " +
+            WeatherEntry.COL_WIND_SPEED + " REAL NOT NULL, " +
+            WeatherEntry.COL_DEGREES + " REAL NOT NULL, " +
 
         // Set up the location column as a foreign key to location table.
-        " FOREIGN KEY (" + WeatherEntry.COLUMN_LOC_KEY + ") REFERENCES " +
+            " FOREIGN KEY (" + WeatherEntry.COL_LOC_KEY + ") REFERENCES " +
         LocationEntry.TABLE_NAME + " (" + LocationEntry._ID + "), " +
 
-        // To assure the application have just one weather entry per day
-        // per location, it's created a UNIQUE constraint with REPLACE strategy
-        " UNIQUE (" + WeatherEntry.COLUMN_DATE + ", " +
-        WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
+        /* To assure the application has just one weather entry per day per
+         * location, it's created with a UNIQUE constraint, and with a REPLACE
+         * strategy. */
+            " UNIQUE (" + WeatherEntry.COL_DATE + ", " +
+            WeatherEntry.COL_LOC_KEY + ") ON CONFLICT REPLACE);";
 
     sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
   }
 
   @Override
-  public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-    // This database is only a cache for online data, so its upgrade policy is
-    // to simply to discard the data and start over
-    // Note that this only fires if you change the version number for your database.
-    // It does NOT depend on the version number for your application.
-    // If you want to update the schema without wiping data, commenting out the next 2 lines
-    // should be your top priority before modifying this method.
+  public void onUpgrade(
+      SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    /* This database is only a cache for online data, so its upgrade policy is
+     * to simply to discard the data and start over. Note that this only fires
+     * if you change the version number for your database. */
     sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + LocationEntry.TABLE_NAME);
     sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME);
     onCreate(sqLiteDatabase);
