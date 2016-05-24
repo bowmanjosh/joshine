9c596bc6593f4e2c7655de15d7428bff43cb955f
Josh Bowman <15678661+bowmanjosh@users.noreply.github.com>
2016-05-24 14:12:23 -0500

    Fill out  buildWeatherLocation() in WeatherEntry.
    This commit fills out the "build this Uri" method that was left unimplemented in Google's starter ContentProvider code. I'm not sure if having these methods in this place (the contract subclasses) is standard code practice, but I understand what they're doing. This commit also uncomments the testBuildWeatherLocation() method in TestWeatherContract, which is a test for the method I filled out.

--- app/src/androidTest/java/com/example/android/sunshine/app/data/TestWeatherContract.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/data/TestWeatherContract.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.sunshine.app.data;
 
+import android.net.Uri;
 import android.test.AndroidTestCase;
 
 /**
@@ -25,26 +26,17 @@ public class TestWeatherContract extends AndroidTestCase {
 
   // intentionally includes a slash to make sure Uri is getting quoted correctly
   private static final String TEST_WEATHER_LOCATION = "/North Pole";
-  private static final long TEST_WEATHER_DATE = 1419033600L;  // December
-  // 20th, 2014
+  // December 20th, 2014
+  private static final long TEST_WEATHER_DATE = 1419033600L;
 
-    /*
-        Students: Uncomment this out to test your weather location function.
-     */
-  //    public void testBuildWeatherLocation() {
-  //        Uri locationUri = WeatherContract.WeatherEntry
-  // .buildWeatherLocation(TEST_WEATHER_LOCATION);
-  //        assertNotNull("Error: Null Uri returned.  You must fill-in
-  // buildWeatherLocation in " +
-  //                        "WeatherContract.",
-  //                locationUri);
-  //        assertEquals("Error: Weather location not properly appended to
-  // the end of the Uri",
-  //                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
-  //        assertEquals("Error: Weather location Uri doesn't match our
-  // expected result",
-  //                locationUri.toString(),
-  //                "content://com.example.android.sunshine
-  // .app/weather/%2FNorth%20Pole");
-  //    }
+  public void testBuildWeatherLocation() {
+    Uri locationUri = WeatherContract.WeatherEntry
+        .buildWeatherLocation(TEST_WEATHER_LOCATION);
+    assertNotNull("Error: Null Uri returned.", locationUri);
+    assertEquals("Error: Weather location not properly appended to the Uri",
+        TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
+    assertEquals("Error: Weather location Uri doesn't match expected result",
+        locationUri.toString(),
+        "content://com.example.android.sunshine.app/weather/%2FNorth%20Pole");
+  }
 }
